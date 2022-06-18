dbed137f706f1198bba327b68d1743ff5b190626
Paweł Kapela <mysio175@gmail.com>
2022-06-18 18:28:43 +0200

    Bootstrap project structure

---
+++ src/main/java/pl/paulkapela/colorchat/ColorchatApplication.java
@@ -0,0 +1,13 @@
+package pl.paulkapela.colorchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/paulkapela/colorchat/ColorchatApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.paulkapela.colorchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
