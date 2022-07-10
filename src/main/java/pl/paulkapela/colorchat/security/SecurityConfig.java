261aea0b446cd1c457d5318a39fbc7903e95dc26
Paweł Kapela <mysio175@gmail.com>
2022-07-10 23:47:56 +0200

    Added BCryptPasswordEncoder bean

--- src/main/java/pl/paulkapela/colorchat/security/SecurityConfig.java
+++ src/main/java/pl/paulkapela/colorchat/security/SecurityConfig.java
@@ -15,6 +15,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
@@ -65,6 +67,11 @@ public class SecurityConfig {
     @Bean
     UserDetailsManager users(DataSource dataSource) {
         return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
