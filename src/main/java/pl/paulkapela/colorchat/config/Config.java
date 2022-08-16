6f1ff473f2c8e8e755600d15df2a81f05dc7dc62
Paweł Kapela <mysio175@gmail.com>
2022-08-16 02:20:21 +0200

    Added the possibility of providing the Bearer token in Swagger

--- src/main/java/pl/paulkapela/colorchat/config/Config.java
+++ src/main/java/pl/paulkapela/colorchat/config/Config.java
@@ -3,10 +3,19 @@ package pl.paulkapela.colorchat.config;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableCaching
@@ -18,6 +27,28 @@ public class Config {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.regex("^(?!/(error).*$).*$"))
+                .build()
+                .securitySchemes(Collections.singletonList(createSecuritySchema()))
+                .securityContexts(Collections.singletonList(createSecurityContext()));
+    }
+
+    private SecurityContext createSecurityContext() {
+        return SecurityContext.builder()
+                .securityReferences(createSecurityRef())
+                .operationSelector(operationContext -> true)
                 .build();
+    }
+
+    private List<SecurityReference> createSecurityRef() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+
+        return Collections.singletonList(new SecurityReference("apiKey", authorizationScopes));
+    }
+
+    private SecurityScheme createSecuritySchema() {
+        return new ApiKey("apiKey", HttpHeaders.AUTHORIZATION, "header");
     }
 }
